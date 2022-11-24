2f293e5a89bae098b96ab8006455f5234fe0d175
MdAsifGhulam786 <asifghulam464@gmail.com>
2022-11-24 21:43:17 +0530

    Created controller dataModels Repository and added a method in controller along with connection with db

---
+++ StudentAdminPortal.API/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using StudentAdminPortal.API.DomainModels;
+using StudentAdminPortal.API.Repositories;
+
+namespace StudentAdminPortal.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentRepository studentRepository;
+        private readonly IMapper mapper;
+
+        public StudentController(IStudentRepository studentRepository, IMapper mapper)
+        {
+            this.studentRepository = studentRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllStudentsAsync()
+        {
+            var students = await studentRepository.GetStudentsAsync();
+
+            return Ok(mapper.Map<List<Student>>(students));
+        }
+    }
+}
